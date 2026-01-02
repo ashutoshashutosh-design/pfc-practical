54a06fdfbf5bf2ade71a6e0aa16f97ad32c00431
ashutoshashutosh-design <ashutosh.ashutosh@rungta.org>
2026-01-02 12:09:24 +0530

    Update 5-1.c

---
+++ 5-1.c
@@ -0,0 +1,36 @@
+// Ashutosh 10318,Calculator using Switch Case
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    switch (op) {
+        case '+':
+            printf("Result = %d", a + b);
+            break;
+        case '-':
+            printf("Result = %d", a - b);
+            break;
+        case '*':
+            printf("Result = %d", a * b);
+            break;
+        case '/':
+            if (b != 0)
+                printf("Result = %d", a / b);
+            else
+                printf("Division by zero not allowed");
+            break;
+        default:
+            printf("Invalid operator");
+    }
+
+    return 0;
+}
