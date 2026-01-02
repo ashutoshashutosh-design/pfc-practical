cf900d91938d9b31822bf2c4212dfbf51d970cac
ashutoshashutosh-design <ashutosh.ashutosh@rungta.org>
2026-01-02 12:09:58 +0530

    Implement odd/even check using if-else

---
+++ 5-2.c
@@ -0,0 +1,17 @@
+// Ashutosh 10318 , odd/even using if-else
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n % 2 == 0) {
+        printf("Number is Even");
+    } else {
+        printf("Number is Odd");
+    }
+
+    return 0;
+}
